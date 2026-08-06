f7d3c81ea6f36999297310073a40422308603e17
Arka Bhattacharya <arkagotham49.28@gmail.com>
2026-08-06 10:29:08 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,22 @@
+class CreditCard:
+    def pay(self, amount):
+        print(f"Payment of Rs{amount} made using Credit Card")
+
+class DebitCard:
+    def pay(self, amount):
+        print(f"Payment of Rs{amount} made using Debit Card")
+
+class UPI:
+    def pay(self, amount):
+        print(f"Payment of Rs{amount} made using UPI")
+
+class NetBanking:
+    def pay(self, amount):
+        print(f"Payment of Rs{amount} made using Net Banking")
+
+class Payment_context:
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def pay(self, amount):
+        self.strategy.pay(amount)
